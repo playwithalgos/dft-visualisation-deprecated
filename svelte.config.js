218d09a996da0f2eff93278c47c231b6fb22f340
Francois Schwarzentruber <fschwarz@schumann.irisa.fr>
2023-09-09 12:45:29 +0200

    path

--- svelte.config.js
+++ svelte.config.js
@@ -12,14 +12,23 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-            // default options are shown. On some platforms
-            // these options are set automatically — see below
-            pages: 'build',
-            assets: 'build',
-            fallback: "index.html",
-            precompress: false,
-            strict: false // otherwise it does not build
-        })
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: "index.html",
+			precompress: false,
+			strict: false // otherwise it does not build
+		})
+		,
+
+
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/dft-visualiation' : '',
+
+		}
+
+
 	}
 };
 
